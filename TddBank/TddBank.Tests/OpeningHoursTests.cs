f4266bea8e3e36567ad7c44a619189325b3808d2
André <AndreR@ppedv.de>
2023-12-04 16:16:31 +0100

    versuch die mutation auf 100% zu bekommen

--- TddBank/TddBank.Tests/OpeningHoursTests.cs
+++ TddBank/TddBank.Tests/OpeningHoursTests.cs
@@ -9,11 +9,13 @@
         [InlineData(2023, 12, 4, 18, 59, true)] //mo
         [InlineData(2023, 12, 4, 19, 00, false)] //mo
         [InlineData(2023, 12, 5, 12, 00, true)] //di
-        [InlineData(2023, 12, 9, 13, 0, true)] //sa
-        [InlineData(2023, 12, 9, 15, 0, false)] //sa
+        [InlineData(2023, 12, 9, 10, 30, true)] //sa
+        [InlineData(2023, 12, 9, 14, 0, false)] //sa
         [InlineData(2023, 12, 10, 12, 0, false)] //so
         [InlineData(2023, 12, 24, 12, 0, false)] 
         [InlineData(2023, 11, 24, 20, 0, false)] 
+        [InlineData(2023, 11, 24, 12, 0, false)] 
+        [InlineData(2023, 11, 30, 12, 0, true)] 
         public void OpeningHours_IsOpen(int y, int M, int d, int h, int m, bool result)
         {
             var dt = new DateTime(y, M, d, h, m, 0);
