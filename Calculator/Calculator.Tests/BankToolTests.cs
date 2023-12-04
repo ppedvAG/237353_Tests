01ae951834c1bdb2f1c2485b345e23a5dbd7bc9e
André <AndreR@ppedv.de>
2023-12-04 12:27:21 +0100

    FluentAssertions und InternalsVisTo für Iban Tool

---
+++ Calculator/Calculator.Tests/BankToolTests.cs
@@ -0,0 +1,39 @@
+﻿using FluentAssertions;
+
+namespace Calculator.Tests
+{
+    public class BankToolTests
+    {
+        [Theory]
+        [InlineData("DE34100500000010050000")]
+        [InlineData("AX2112345600000785")]
+        [InlineData("AL47212110090000000235698741")]
+        [InlineData("AD1200012030200359100100")]
+        [InlineData("AT611904300234573201")]
+        
+        public void IsValidIban_with_valid_IBANs(string iban)
+        {
+            var result = BankTool.IsValidIban(iban);
+
+            //Assert.True(result);
+            result.Should().BeTrue();
+
+            "Text".Should().NotContain("#").And.Contain("x");
+        }
+
+        [Theory]
+        [InlineData("")]
+        [InlineData(" ")]
+        //[InlineData(null)]
+        [InlineData("DE1")] //zu kurz
+        [InlineData("9912345")] //fängt nicht mit Buchstaben an
+        [InlineData("BBAA345")] //zeichen 3 und 4 sind keine Zahlen
+        public void IsValidIban_with_invalid_IBANs(string iban)
+        {
+            var result = BankTool.IsValidIban(iban);
+
+            result.Should().BeFalse();
+        }
+
+    }
+}
---
+++ Calculator/Calculator/BankTool.cs
@@ -0,0 +1,50 @@
+﻿using System.Runtime.CompilerServices;
+using System.Text;
+using System.Text.RegularExpressions;
+
+[assembly: InternalsVisibleTo("Calculator.Tests")]
+
+namespace Calculator
+{
+    public class BankTool
+    {
+        internal static bool IsValidIban(string iban)
+        {
+            // Entfernen von Leerzeichen und Umwandeln in Großbuchstaben
+            iban = iban.Replace(" ", "").ToUpper();
+
+            // Prüfen, ob die IBAN die richtige Länge hat
+            if (iban.Length < 4)
+                return false;
+
+            // Prüfen, ob die ersten vier Zeichen Buchstaben sind
+            if (!Regex.IsMatch(iban.Substring(0, 2), @"^[A-Z]+$"))
+                return false;
+
+            // Prüfen, ob die folgenden beiden Zeichen Ziffern sind
+            if (!Regex.IsMatch(iban.Substring(2, 2), @"^[0-9]+$"))
+                return false;
+
+            // IBAN umsortieren und in Zahl umwandeln
+            string rearrangedIban = iban.Substring(4) + iban.Substring(0, 4);
+            StringBuilder numericIban = new StringBuilder();
+
+            foreach (char c in rearrangedIban)
+            {
+                if (char.IsDigit(c))
+                    numericIban.Append(c);
+                else
+                    numericIban.Append((c - 'A' + 10).ToString());
+            }
+
+            // IBAN-Validierung: Modulo-97-Prüfung
+            int remainder = 0;
+            foreach (char digit in numericIban.ToString())
+            {
+                remainder = (remainder * 10 + (digit - '0')) % 97;
+            }
+
+            return remainder == 1;
+        }
+    }
+}
