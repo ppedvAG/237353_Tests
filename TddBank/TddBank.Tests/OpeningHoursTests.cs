37d6b1e8884f394d01817eb82e1c577367bcc497
André <AndreR@ppedv.de>
2023-12-04 15:15:47 +0100

    OpeningHours Tests

---
+++ TddBank/TddBank.Tests/OpeningHoursTests.cs
@@ -0,0 +1,23 @@
+﻿namespace TddBank.Tests
+{
+    public class OpeningHoursTests
+    {
+        [Theory]
+        [InlineData(2023, 12, 4, 10, 30, true)]//mo
+        [InlineData(2023, 12, 4, 10, 29, false)]//mo
+        [InlineData(2023, 12, 4, 10, 31, true)] //mo
+        [InlineData(2023, 12, 4, 18, 59, true)] //mo
+        [InlineData(2023, 12, 4, 19, 00, false)] //mo
+        [InlineData(2023, 12, 5, 12, 00, true)] //di
+        [InlineData(2023, 12, 9, 13, 0, true)] //sa
+        [InlineData(2023, 12, 9, 15, 0, false)] //sa
+        [InlineData(2023, 12, 10, 12, 0, false)] //so
+        public void OpeningHours_IsOpen(int y, int M, int d, int h, int m, bool result)
+        {
+            var dt = new DateTime(y, M, d, h, m, 0);
+            var oh = new OpeningHours();
+
+            Assert.Equal(result, oh.IsOpen(dt));
+        }
+    }
+}
---
+++ TddBank/TddBank/OpeningHours.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace TddBank
+{
+    public class OpeningHours
+    {
+        public bool IsOpen(DateTime dateTime)
+        {
+            // Check if it's Monday and the time is between 10:30 and 19:00
+            if (dateTime.DayOfWeek != DayOfWeek.Sunday && 
+                dateTime.DayOfWeek != DayOfWeek.Saturday &&
+                dateTime.TimeOfDay >= new TimeSpan(10, 30, 0) &&
+                dateTime.TimeOfDay < new TimeSpan(19, 0, 0))
+            {
+                return true;
+            }
+
+            // Check if it's Saturday and the time is between 10:30 and 16:00
+            if (dateTime.DayOfWeek == DayOfWeek.Saturday &&
+                dateTime.TimeOfDay >= new TimeSpan(10, 30, 0) &&
+                dateTime.TimeOfDay < new TimeSpan(14, 0, 0))
+            {
+                return true;
+            }
+
+            // No other opening hours specified, default to closed
+            return false;
+        }
+    }
+}
