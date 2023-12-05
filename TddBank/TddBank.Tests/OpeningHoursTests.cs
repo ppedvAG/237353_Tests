c6795452f5f93e0f80a3bcf514819fe45712bf40
André <AndreR@ppedv.de>
2023-12-05 10:18:07 +0100

    Fakes mit MS Fakes

--- TddBank/TddBank.Tests/OpeningHoursTests.cs
+++ TddBank/TddBank.Tests/OpeningHoursTests.cs
@@ -1,4 +1,7 @@
-﻿namespace TddBank.Tests
+﻿using Microsoft.QualityTools.Testing.Fakes;
+using System.Diagnostics.Metrics;
+
+namespace TddBank.Tests
 {
     public class OpeningHoursTests
     {
@@ -12,16 +15,40 @@
         [InlineData(2023, 12, 9, 10, 30, true)] //sa
         [InlineData(2023, 12, 9, 14, 0, false)] //sa
         [InlineData(2023, 12, 10, 12, 0, false)] //so
-        [InlineData(2023, 12, 24, 12, 0, false)] 
-        [InlineData(2023, 11, 24, 20, 0, false)] 
-        [InlineData(2023, 11, 24, 12, 0, false)] 
-        [InlineData(2023, 11, 30, 12, 0, true)] 
+        [InlineData(2023, 12, 24, 12, 0, false)]
+        [InlineData(2023, 11, 24, 20, 0, false)]
+        [InlineData(2023, 11, 24, 12, 0, true)]
+        [InlineData(2023, 11, 30, 12, 0, true)]
         public void OpeningHours_IsOpen(int y, int M, int d, int h, int m, bool result)
         {
             var dt = new DateTime(y, M, d, h, m, 0);
             var oh = new OpeningHours();
 
             Assert.Equal(result, oh.IsOpen(dt));
+        }
+
+        [Fact]
+        public void IsWeekend()
+        {
+            using (var context = ShimsContext.Create())
+            {
+                var oh = new OpeningHours();
+                System.Fakes.ShimDateTime.NowGet = () => new DateTime(2023, 12, 4);
+                Assert.False(oh.IsWeekend());
+                System.Fakes.ShimDateTime.NowGet = () => new DateTime(2023, 12, 5);
+                Assert.False(oh.IsWeekend());
+                System.Fakes.ShimDateTime.NowGet = () => new DateTime(2023, 12, 6);
+                Assert.False(oh.IsWeekend());
+                System.Fakes.ShimDateTime.NowGet = () => new DateTime(2023, 12, 7);
+                Assert.False(oh.IsWeekend());
+                System.Fakes.ShimDateTime.NowGet = () => new DateTime(2023, 12, 8);
+                Assert.False(oh.IsWeekend());
+
+                System.Fakes.ShimDateTime.NowGet = () => new DateTime(2023, 12, 9);
+                Assert.True(oh.IsWeekend());
+                System.Fakes.ShimDateTime.NowGet = () => new DateTime(2023, 12, 10);
+                Assert.True(oh.IsWeekend());
+            }
         }
     }
 }
--- TddBank/TddBank/OpeningHours.cs
+++ TddBank/TddBank/OpeningHours.cs
@@ -4,13 +4,26 @@ namespace TddBank
 {
     public class OpeningHours
     {
+        public bool IsWeekend()
+        {
+            return DateTime.Now.DayOfWeek == DayOfWeek.Sunday ||
+                   DateTime.Now.DayOfWeek == DayOfWeek.Saturday;
+        }
+
+        public bool IsNowOpen()
+        {
+            return IsOpen(DateTime.Now);
+        }
+
         public bool IsOpen(DateTime dateTime)
         {
+
+
             if (dateTime.Month == 12 && dateTime.Day == 24)
                 return false;
 
             // Check if it's Monday and the time is between 10:30 and 19:00
-            if (dateTime.DayOfWeek != DayOfWeek.Sunday && 
+            if (dateTime.DayOfWeek != DayOfWeek.Sunday &&
                 dateTime.DayOfWeek != DayOfWeek.Saturday &&
                 dateTime.TimeOfDay >= new TimeSpan(10, 30, 0) &&
                 dateTime.TimeOfDay < new TimeSpan(19, 0, 0))
